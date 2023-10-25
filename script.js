860e97eaae3cdf14f2e71eea3a0ca74548a09478
Nikita <kavtsevichnikita@gmail.com>
2023-10-25 20:34:14 +0200

    Food blog after 4 sections

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".contact-form");
+    const sendButton = document.querySelector("#send");
+
+    sendButton.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const name = document.querySelector("#name").value;
+        const email = document.querySelector("#email").value;
+        const url = document.querySelector("#url").value;
+        const keywords = document.querySelector("#keywords").value;
+        const pastries = document.querySelector("#pastries-checkbox").checked;
+        const message = document.querySelector("textarea[name='message']").value;
+
+        let formDataArray = JSON.parse(localStorage.getItem("formDataArray")) || [];
+
+        const formData = {
+            name,
+            email,
+            url,
+            keywords,
+            pastries,
+            message,
+        };
+
+        if (name === "" || email === "" || url === "" || message === "") {
+            alert("All fields must be filled out!");
+        } else {
+            formDataArray.push(formData);
+            localStorage.setItem("formDataArray", JSON.stringify(formDataArray));
+    
+            window.location.href = "./article2.html";
+        }
+    });
+});
+
+    const contactForm = document.getElementById('contact-form');
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const urlInput = document.getElementById('url');
+    const keywordsInput = document.getElementById('keywords');
+    const pastriesCheckbox = document.getElementById('pastries-checkbox');
+    const messageTextarea = document.querySelector('textarea');
+    const resetButton = document.getElementById('reset');
+
+
+    resetButton.addEventListener('click', function () {
+        nameInput.value = '';
+        emailInput.value = '';
+        urlInput.value = '';
+        keywordsInput.value = '';
+        pastriesCheckbox.checked = false;
+        messageTextarea.value = '';
+    });
